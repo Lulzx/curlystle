d11d21efc0b8a17d8a840e26f6623a771c4ac213
Lulzx <wiserishabh@gmail.com>
2018-07-28 19:24:34 +0530

    making code correct

--- src/curlystle.cr
+++ src/curlystle.cr
@@ -5,7 +5,6 @@ bot = Tourmaline::Bot::Client.new(ENV["API_KEY"])
 bot.command("echo") do |message, params|
   text = params.join(" ")
   bot.send_message(message.chat.id, text)
-  bot.delete_message(message.chat.id, message.message_id)
 end
 
 bot.poll
