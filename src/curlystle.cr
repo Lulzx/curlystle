cd6011915e3e46e6ff0e1d42095a3f0475d02add
Lulzx <wiserishabh@gmail.com>
2018-07-28 19:49:00 +0530

    Echo

--- src/curlystle.cr
+++ src/curlystle.cr
@@ -1,10 +1,8 @@
 require "tourmaline"
 
 bot = Tourmaline::Bot::Client.new(ENV["API_KEY"])
-
-bot.on(TGBot::UpdateAction::Text) do |update|
+bot.on(Tourmaline::Bot::UpdateAction::Text) do |update|
   text = update.message.not_nil!.text.not_nil!
   puts "TEXT: #{text}"
 end
-
 bot.poll
